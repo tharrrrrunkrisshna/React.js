c6f0bb11414379faa267d67b1f42d2f2651c8d65
tharrrrrunkrisshna <43927016+tharrrrrunkrisshna@users.noreply.github.com>
2023-04-05 15:22:27 +0530

    Create App.js

---
+++ Day1/Classwork/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Javascript from './Javascript';
+
+function App() {
+  return (
+    <div>
+      <center>
+       <Javascript/>
+       </center>
+    </div>
+  );
+}
+
+export default App;
